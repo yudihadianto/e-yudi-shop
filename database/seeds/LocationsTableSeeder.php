7a29ae6c4c10bbbd94e036a6a236d3920a35beff
Yudi Hadianto <yudihadiyanto7@gmail.com>
2021-12-08 00:34:09 +0700

    a

---
+++ database/seeds/LocationsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LocationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $daftarProvinsi = RajaOngkir::provinsi()->all();
+
+
+    }
+}
